a3eef64909b73b13c4d3a8ebc1f46e949b2e0eec
Bridgit-wairimu <kirikabetty@gmail.com>
2020-11-30 16:22:51 +0300

    deleted the models folder

--- app/models/sources.py
+++
@@ -1,9 +0,0 @@
-class Sources:
-    """
-    source class to define Sources Objects
-    """
-
-    def __init__(self,name,author,content):
-        self.name = name
-        self.author = author
-        self.content = content
